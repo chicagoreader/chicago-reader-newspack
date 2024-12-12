06e264686686b9dfa3f6576dc2fffadb04e17ba5
Brian Boyer <brian@hackerjournalist.net>
2024-12-12 14:15:07 -0600

    initial setup

---
+++ chicago-reader-newspack.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Chicago Reader Newspack
+ * Plugin URI: https://github.com/chicagoreader/chicago-reader-newspack
+ * Description: Custom styles and tweaks to Newspack
+ * Version: 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+function chicago_reader_scripts() {
+	wp_enqueue_style(
+		'chicago-reader',
+		WP_PLUGIN_URL . '/chicago-reader-newspack/css/chicago-reader.css',
+		array(),
+		filemtime( WP_PLUGIN_DIR . '/chicago-reader-newspack/css/chicago-reader.css' ),
+	);
+}
+add_action('wp_enqueue_scripts', 'chicago_reader_scripts', 99);
